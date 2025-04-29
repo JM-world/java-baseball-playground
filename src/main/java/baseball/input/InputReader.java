13432ca62335a8aa1d98f3938593d61e691c4a00
linxiangkaiho <unhoiylaw@naver.com>
2025-04-29 23:03:44 +0900

    feat: 사용자 입력을 받기 위해 UserInput 추가 및 Scanner 격리를 위한 InputReader 추가

---
+++ src/main/java/baseball/input/InputReader.java
@@ -0,0 +1,11 @@
+package baseball.input;
+
+import java.util.Scanner;
+
+public class InputReader {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static String readLine() {
+        return sc.nextLine();
+    }
+}
---
+++ src/main/java/baseball/input/UserInput.java
@@ -0,0 +1,9 @@
+package baseball.input;
+
+public class UserInput {
+
+    public static String getUserInput() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        return InputReader.readLine();
+    }
+}
