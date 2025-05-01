74803aaead7428d5391435baaf653704664e08a2
linxiangkaiho <unhoiylaw@naver.com>
2025-05-02 00:37:56 +0900

    feat: 출력 유틸클래스 생성

---
+++ src/main/java/baseball/utils/PrintUtil.java
@@ -0,0 +1,11 @@
+package baseball.utils;
+
+public class PrintUtil {
+    public static void print(String input) {
+        System.out.print(input);
+    }
+
+    public static void println(String input) {
+        System.out.println(input);
+    }
+}
