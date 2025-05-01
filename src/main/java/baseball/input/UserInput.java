a715408f3cbbd63fe692b32d82398264fa7dd0fb
linxiangkaiho <unhoiylaw@naver.com>
2025-05-02 00:37:06 +0900

    feat: 사용자 입력 시 유효성 검사 및 숫자 리스트 변환 로직 추가

--- src/main/java/baseball/input/UserInput.java
+++ src/main/java/baseball/input/UserInput.java
@@ -1,9 +1,18 @@
 package baseball.input;
 
+import baseball.view.InputView;
+
+import java.util.List;
+
+import static baseball.utils.Parser.toIntegerList;
+
 public class UserInput {
 
-    public static String getUserInput() {
-        System.out.print("숫자를 입력해 주세요 : ");
-        return InputReader.readLine();
+    public static List<Integer> getUserInput() {
+        InputView.numberInputInfo();
+        String input = InputReader.readLine();
+        // 숫자 검증
+        InputValidator.validateFirstGameAll(input);
+        return toIntegerList(input);
     }
 }
