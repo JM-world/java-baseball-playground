5ee20ba281251d689d1580cc68c1f40b44f62713
linxiangkaiho <unhoiylaw@naver.com>
2025-05-02 00:38:39 +0900

    feat: 문자열 -> 정수 리스트 변환 메소드 로직 추가

---
+++ src/main/java/baseball/utils/Parser.java
@@ -0,0 +1,13 @@
+package baseball.utils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+    public static List<Integer> toIntegerList(String input) {
+        return input.chars()
+                .map(Character :: getNumericValue)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
