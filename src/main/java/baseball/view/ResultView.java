15cb22ba4244790662f9bafabd4d5f2ca751fc4f
linxiangkaiho <unhoiylaw@naver.com>
2025-05-02 00:40:41 +0900

    feat(ResultView): 3스트라이크 시 결과 화면 추가

---
+++ src/main/java/baseball/view/ResultView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import baseball.utils.PrintUtil;
+
+public class ResultView {
+    public static void isCorrect() {
+            PrintUtil.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
