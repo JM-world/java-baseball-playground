a1b8a78420fd6fb4ee3a79cca2986a62e23e774c
linxiangkaiho <unhoiylaw@naver.com>
2025-05-07 22:26:20 +0900

    feat(InputView): 숫자 입력문구 및 재시작 문구를 출력하는 InputView 추가

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class InputView {
+
+    public static void numberInputInfo() {
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+
+    public static void restartInfo() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
