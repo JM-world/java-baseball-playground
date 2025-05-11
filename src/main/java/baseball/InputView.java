89daff723b0fd0921f72c22c214b9d681b91a503
linxiangkaiho <unhoiylaw@naver.com>
2025-05-11 22:34:52 +0900

    feat(InputView): 플레이어가 유효하지 않은 값을 입력 시 문구 추가

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -6,6 +6,10 @@ public class InputView {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
+    public static void invalidNumberInfo() {
+        System.out.print("유효하지 않은 값입니다.");
+    }
+
     public static void restartInfo() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
