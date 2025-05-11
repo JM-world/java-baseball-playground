f3df4110d83b6c0974c6260c531d0e0cd59c6134
linxiangkaiho <unhoiylaw@naver.com>
2025-05-11 22:32:45 +0900

    feat(NumberValidationUtils): 플레이어 입력값 검증 로직 추가, 컴퓨터 또는 플레이어 검증 주석 구분

--- src/main/java/baseball/NumberValidationUtils.java
+++ src/main/java/baseball/NumberValidationUtils.java
@@ -4,23 +4,43 @@ import java.util.HashSet;
 import java.util.List;
 
 public class NumberValidationUtils {
-    // 중복 값이 있는지 확인
+
+    // (플레이어) 입력 값 검증
+    public static boolean playerNumberIsNotInvalid(String numbers) {
+        return validateThreeDigitRange1to9(numbers) && isNotDuplicationOfPlayer(numbers);
+    }
+
+    // (컴퓨터) 입력 값 검증
+    public static boolean computerNumberIsNotInvalid(List<Integer> numbers) {
+        return isNotDuplication(numbers)
+                && isRequirementLength(numbers)
+                && isItEachNumber(numbers);
+    }
+
+    // (컴퓨터 수 검증) 중복 값이 있는지 확인
     public static boolean isNotDuplication(List<Integer> numbers) {
         return new HashSet<>(numbers).size() == numbers.size();
     }
 
-    // n만큼의 길이인지 확인
-    public static boolean isRequirementLength(List<Integer> numbers, int n) {
-        return numbers.size() == n;
+    // (컴퓨터 수 검증) n 만큼의 길이인지 확인
+    public static boolean isRequirementLength(List<Integer> numbers) {
+        return numbers.size() == DigitConstants.NUMBER_SIZE;
     }
 
-    // 각 자리의 수가 1부터 9까지인지 확인
+    // (컴퓨터 수 검증) 각 자리의 수가 1부터 9까지인지 확인
     public static boolean isItEachNumber(List<Integer> numbers) {
         return numbers.stream().allMatch(number -> number >= 1 && number <= 9);
     }
 
-    // 사용자에게 입력 받을 시, 해당 문자열이 1부터 9까지 3자리 수인지 확인
+    // (플레이어 검증) 해당 값들이 1부터 9까지 3자리 수인지 확인
     public static boolean validateThreeDigitRange1to9(String numbers) {
         return numbers.matches("^[1-9]{3}$");
+    }
+
+    // (플레이어 검증) 중복 값이 있는지 확인
+    public static boolean isNotDuplicationOfPlayer(String numbers) {
+        return numbers.chars()
+                .distinct()
+                .count() == numbers.length();
     }
 }
--- src/test/java/study/NumberValidationUtilsTest.java
+++ src/test/java/study/NumberValidationUtilsTest.java
@@ -1,11 +1,11 @@
 package study;
 
-import baseball.Balls;
-import baseball.DigitConstants;
 import baseball.NumberValidationUtils;
 import baseball.RandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,65 +15,93 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberValidationUtilsTest {
 
+    // 플레이어 입력값 검증 전체
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789"})
+    @DisplayName("플레이어는 1에서 9까지 서로 다른 임의의 수 3개를 입력")
+    void playerNumber(String playerNumber) {
+        // 플레이어 검증 전체 메소드
+        assertThat(NumberValidationUtils.playerNumberIsNotInvalid(playerNumber)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789"})
+    @DisplayName("플레이어 입력값 범위 및 자리수 검증1")
+    void 각_숫자가_1부터_9사이이고_3자리이면_true(String playerNumber) {
+        assertThat(NumberValidationUtils.validateThreeDigitRange1to9(playerNumber)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a23", "1234", "12", "023", "", " "})
+    @DisplayName("플레이어 입력값 중복 검증2")
+    void 숫자가_1부터_9사이가_아니거나_3자리가_아니면_false(String playerNumber) {
+        assertThat(NumberValidationUtils.validateThreeDigitRange1to9(playerNumber)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789"})
+    @DisplayName("플레이어 입력값 중복 검증1")
+    void 중복이_없으면_true(String playerNumber) {
+        assertThat(NumberValidationUtils.isNotDuplicationOfPlayer(playerNumber)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"111", "122"})
+    @DisplayName("플레이어 입력값 중복 검증2")
+    void 중복이_있으면_false(String playerNumber) {
+        assertThat(NumberValidationUtils.isNotDuplicationOfPlayer(playerNumber)).isFalse();
+    }
+
+    // 컴퓨터 랜덤값 검증 전체
     @Test
     @DisplayName("컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 생성")
     void computerNumber() {
         List<Integer> numbers = RandomNumberGenerator.createNumbers();
-        assertThat(NumberValidationUtils.isNotDuplication(numbers)).isTrue();
-        assertThat(NumberValidationUtils.isRequirementLength(numbers, DigitConstants.NUMBER_SIZE)).isTrue();
-        assertThat(NumberValidationUtils.isItEachNumber(numbers)).isTrue();
+        // 컴퓨터 검증 전체 메소드
+        assertThat(NumberValidationUtils.computerNumberIsNotInvalid(numbers)).isTrue();
     }
 
     @Test
-    @DisplayName("중복 검증1")
+    @DisplayName("컴퓨터 랜덤 값 중복 검증1")
     void 중복이_없으면_true() {
         assertThat(NumberValidationUtils.isNotDuplication(Arrays.asList(1, 2, 3))).isTrue();
     }
 
     @Test
-    @DisplayName("중복 검증2")
+    @DisplayName("컴퓨터 랜덤 값 중복 검증2")
     void 중복이_있으면_false() {
         assertThat(NumberValidationUtils.isNotDuplication(Arrays.asList(1, 1, 2))).isFalse();
     }
 
     @Test
-    @DisplayName("중복 검증3")
+    @DisplayName("컴퓨터 랜덤 값 중복 검증3")
     void 빈_리스트는_true() {
         assertThat(NumberValidationUtils.isNotDuplication(new ArrayList<>())).isTrue();
     }
 
     @Test
-    @DisplayName("길이 검증1")
+    @DisplayName("컴퓨터 랜덤 값 길이 검증1")
     void 길이가_요구사항숫자_3이면_true() {
-        assertThat(NumberValidationUtils.isRequirementLength(
-                Arrays.asList(1, 2, 3),
-                DigitConstants.NUMBER_SIZE)
-        ).isTrue();
+        assertThat(NumberValidationUtils.isRequirementLength(Arrays.asList(1, 2, 3))).isTrue();
     }
 
     @Test
-    @DisplayName("길이 검증2")
+    @DisplayName("컴퓨터 랜덤 값 길이 검증2")
     void 길이가_요구사항숫자_3이_아니면_false() {
         // 3보다 짧은 길이인 경우
-        assertThat(NumberValidationUtils.isRequirementLength(
-                Arrays.asList(1, 2),
-                DigitConstants.NUMBER_SIZE)
-        ).isFalse();
+        assertThat(NumberValidationUtils.isRequirementLength(Arrays.asList(1, 2))).isFalse();
         // 3보다 긴 길이인 경우
-        assertThat(NumberValidationUtils.isRequirementLength(
-                Arrays.asList(1, 2, 3, 4),
-                DigitConstants.NUMBER_SIZE)
-        ).isFalse();
+        assertThat(NumberValidationUtils.isRequirementLength(Arrays.asList(1, 2, 3, 4))).isFalse();
     }
 
     @Test
-    @DisplayName("숫자 범위 검증1")
+    @DisplayName("컴퓨터 랜덤 값 숫자 범위 검증1")
     void 각_숫자의_범위가_1부터_9사이라면_true() {
         assertThat(NumberValidationUtils.isItEachNumber(Arrays.asList(1, 5, 9))).isTrue();
     }
 
     @Test
-    @DisplayName("숫자 범위 검증2")
+    @DisplayName("컴퓨터 랜덤 값 숫자 범위 검증2")
     void 각_숫자의_범위가_1부터_9사이가_아닌경우_false() {
         assertThat(NumberValidationUtils.isItEachNumber(Arrays.asList(-1, 5, 10))).isFalse();
     }
